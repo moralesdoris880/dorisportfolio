2eeecbcfd6363ab05172f5ab84ea5066b876a987
Doris Morales <moralesdoris880@gmail.com>
2022-04-26 10:43:47 -0400

    Resume css changed

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,10 @@
+import '../css/resume.css'; 
+
 function Resume(){
     return(
-        <div>
-            <div>
+        <div id="resumebox">
+            <div id="resumecontent">
+                <a className="btn" href="/media/resume2.pdf" download><p>download</p></a>
                 <img id="resume" src="/media/resume.png" alt="resume"/>
             </div>
         </div>
