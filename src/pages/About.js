e9603bffb51dba8508e7f32001beeedfde94065d
Doris Morales <moralesdoris880@gmail.com>
2022-05-02 13:36:49 -0400

    About page h1 css change

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,13 +7,13 @@ function About(){
                 <div id="aboutimg"><img src="/media/doris.png" alt="doris"/></div>
                 <div id="aboutbio">
                     <div>
-                        <div id="abouttitlebox"><h1 id="abouttitle">about me</h1></div>
+                        <h1 id="abouttitle">About Me</h1>
                         <div id="aboutext">
                             <p >Hi! Nice to meet you. I am a Full Stack Developer based in NYC. I focus on building web applications. I love learning about new technologies and creating innovative solutions. When I am not coding I love to make art with all types of mediums. </p>
                         </div>
                     </div>
                     <div id="tech">
-                        <div id="techtitlebox"><h1 id="techtitle">technologies</h1></div>
+                        <h1 id="techtitle">Technologies</h1>
                         <div id="techlist">
                             <div className="techimg"><p>> HTML</p></div>
                             <div className="techimg"><p>> CSS</p></div>
@@ -25,7 +25,7 @@ function About(){
                     </div>
 
                     <div id="aboutbottomcontent">
-                    <div id="connectbox"><h1 id="connecttitle">socials</h1></div>
+                    <h1 id="connecttitle">Socials</h1>
                     <div id="connectlist"> 
                         <a href="https://www.linkedin.com/in/dorisvmorales/" className="connecttab"><p>> LinkedIn</p></a>
                         <a href="https://github.com/moralesdoris880" className="connecttab"><p>> Github</p></a>
